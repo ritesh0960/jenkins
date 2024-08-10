f193363cffa899e96e217946f35e4b72c3d5ece6
ritesh0960 <riteshraushan0960@gmail.com>
2024-08-10 14:22:17 +0530

    first commit

---
+++ src/main/java/com/jenkins/practice/PracticeApplication.java
@@ -0,0 +1,29 @@
+package com.jenkins.practice;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class PracticeApplication {
+
+	public static void main(String[] args) {
+		 ILoggerFactory loggerFactory= LoggerFactory.getILoggerFactory();
+		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+	@RestController
+	class Controller{
+
+		@GetMapping("/hello")
+		public String getHello(){
+			return "Hello world";
+		}
+
+	}
+
+}
